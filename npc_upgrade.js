8e1cb530d103f556abd58cd25c4ec4b46d5e5edb
Yah Avatar <yah.avatar@gmail.com>
2021-07-24 22:47:02 +0000

    Completed basic Non-Humanoid scaling with AC.

--- npc_upgrade.js
+++ npc_upgrade.js
@@ -1,3 +1,24 @@
+/*
+    Items (Magic Items)
+    Loot
+    
+
+    Non-Human NPCs (Creatures)
+        Features
+        
+        
+        
+    New Options
+        * Have Percentage chance/cr for each selected creature to become a definite class
+            Checkbox    and     dropdown percentage picker
+            
+            
+    Add more randomness to HP
+    
+    Last Update: 2021.07.24
+*/
+
+
 dialog_options();
 
 async function main(opt){
@@ -17,38 +38,69 @@ async function main(opt){
         tok.items_to_add = [];
         tok.items_to_delete = [];
         tok.opt = opt;
-
-        //Get token data before adjustment
-        tok.alignment = TADD.details.alignment;                         //Alignment
-        tok.cr_old = TADD.details.cr;                                   //NPC CR old
-        if (tok.cr_old < 1){ tok.cr_old = 1; }
-        tok.adjust_factor = (tok.cr_old + opt.npc_cr) / tok.cr_old;     //Adjust Factor
-        tok.cr_new = parseInt(tok.cr_old * tok.adjust_factor + 0.5);    //NPC CR new
-        tok.data_to_update[AD+"details.cr"] = tok.cr_new;
-        tok.race = TADD.details.race
-        tok.type = TADD.details.type.value;                             //Type of NPC (Humanoid, etc)
         
-        tok.spellcasting = await can_cast_spells(token);                //Can cast spells?
+        //CR and adjustment_factor
+        tok.cr = TADD.details.cr;
+        tok.cr_orig = TADD.details.cr_orig;
+        if (tok.cr_orig){
+            console.log("   CR True")
+            tok.abilities.cha = TADD.abilities.cha.orig;
+            tok.abilities.con = TADD.abilities.con.orig;
+            tok.abilities.dex = TADD.abilities.dex.orig;
+            tok.abilities.int = TADD.abilities.int.orig;
+            tok.abilities.str = TADD.abilities.str.orig;
+            tok.abilities.wis = TADD.abilities.wis.orig;
+        } else {
+            console.log("   CR False")
+            tok.cr_orig = tok.cr;
+            if (tok.cr_orig < 1){ tok.cr_orig = 1; }
+            tok.data_to_update[AD+"details.cr_orig"] = tok.cr_orig;
+            tok.data_to_update[AD+"abilities.cha.orig"] = TADD.abilities.cha.value;
+            tok.data_to_update[AD+"abilities.con.orig"] = TADD.abilities.con.value;
+            tok.data_to_update[AD+"abilities.dex.orig"] = TADD.abilities.dex.value;
+            tok.data_to_update[AD+"abilities.int.orig"] = TADD.abilities.int.value;
+            tok.data_to_update[AD+"abilities.str.orig"] = TADD.abilities.str.value;
+            tok.data_to_update[AD+"abilities.wis.orig"] = TADD.abilities.wis.value;
+            tok.abilities.cha = TADD.abilities.cha.value;
+            tok.abilities.con = TADD.abilities.con.value;
+            tok.abilities.dex = TADD.abilities.dex.value;
+            tok.abilities.int = TADD.abilities.int.value;
+            tok.abilities.str = TADD.abilities.str.value;
+            tok.abilities.wis = TADD.abilities.wis.value;
+        }
+        tok.cr_new = tok.cr + opt.cr_change;
+        tok.data_to_update[AD+"details.cr"] = tok.cr_new;
+        tok.cr_change_since_orig = tok.cr_new - tok.cr_orig;
+        tok.adjust_factor = tok.cr_new / tok.cr;
+
+        //Misc Token info
+        tok.alignment = TADD.details.alignment;                             //Alignment
+        tok.race = TADD.details.race
+        tok.type = TADD.details.type.value;                                 //Type of NPC (Humanoid, etc)
+
+        //Spellcasting
+        tok.spellcasting = await can_cast_spells(token);                    //Can cast spells?
         if (tok.spellcasting){
             tok.spellcaster_type = TADD.attributes.spellcasting;
         } else {
             tok.spellcaster_type = false;
         }
-        
+
         //Choose Template
         tok.template = await template_choose(tok);
         console.log(tok);
 
         //Adjust Abilities (Stats)
         if (opt.adjust_abilities){
-            tok.abilities.add_points = Math.ceil(opt.npc_cr / 3);
+            tok.abilities.add_points = Math.ceil(parseInt(tok.cr_change_since_orig / 3)); //Math.ceil(opt.cr_change / 3);
             for (let a of tok.template.attributes){
-                tok.abilities[a] = parseInt(TADD.abilities[a].value) + tok.abilities.add_points;
+                tok.abilities[a] = tok.abilities[a] + tok.abilities.add_points;
                 if (tok.abilities[a] > 25){ tok.abilities[a] = 25; }
+                if (tok.abilities[a] < 1){ tok.abilities[a] = 1; }
                 tok.data_to_update[AD+"abilities." + a + ".value"] = tok.abilities[a];
             }
         }
-        
+
         //Adjust HP
         if (opt.adjust_hp){
             let hp = TADD.attributes.hp.max;
@@ -61,7 +113,7 @@ async function main(opt){
             for (let m of ["burrow","climb","fly","swim","walk"]){
                 let cur_m = TADD.attributes.movement[m];
                 if (cur_m > 0){
-                    tok["movement_" + m] = parseInt(cur_m + opt.npc_cr);
+                    tok["movement_" + m] = parseInt(cur_m + opt.cr_change);
                     tok.data_to_update[AD+"attributes.hp.movement." + m] = tok["movement_" + m];
                 }
             }
@@ -69,13 +121,12 @@ async function main(opt){
 
         //Is npc a humanoid
         if (tok.type == "humanoid"){
-            
             //Armor
             if (opt.adjust_armor){
                 let armor_plus_str = await armor_get(tok);
                 tok.items_to_add.push(["dnd5e.items", armor_plus_str])    
             }
-            
+
             //Weapons
             if (opt.adjust_weapons){
                 let weapon_melee_str = await weapon_melee_get(tok);
@@ -83,7 +134,7 @@ async function main(opt){
                 let weapon_range_str = await weapon_range_get(tok);
                 tok.items_to_add.push(["dnd5e.items", weapon_range_str]);
             }
-            
+
             //Spells
             if (tok.spellcasting){
                 let new_spelllevel = parseInt(tok.cr_new * 1.25);
@@ -93,35 +144,68 @@ async function main(opt){
                 tok.data_to_update[AD+"details.spellLevel"] = new_spelllevel;
                 
                 //Load spells based on template
-                let spells = spells_get(tok);
                 
+                console.log(tok);
                 
+                let spells = await spells_get(tok);
+                console.log(spells);
+                for (let spell of spells){
+                    //console.log(spell);
+                    tok.items_to_add.push(["dnd5e.spells", spell]);
+                }
             }
-            
         } else {
-            //Not Humanoid
+            //Non-Humanoids
+           
+            //Adjust AC
+            tok.ac_orig = TADD.attributes.ac.orig;
+            tok.ac = TADD.attributes.ac.value;
+            if (tok.cr_new > 0){
+                tok.ac_adjust = parseInt((tok.cr_new - tok.cr_orig)/2);
+            } else {
+                tok.ac_adjust = 0
+            }
+            if (tok.ac_orig){
+                console.log("   True")
+                tok.data_to_update[AD+"attributes.ac.min"] = tok.ac_orig + tok.ac_adjust;
+                tok.data_to_update[AD+"attributes.ac.value"] = tok.ac_orig + tok.ac_adjust;
+            } else {
+                //Create
+                console.log("   False")
+                tok.data_to_update[AD+"attributes.ac.orig"] = tok.ac;
+                tok.data_to_update[AD+"attributes.ac.min"] = tok.ac + tok.ac_adjust;
+                tok.data_to_update[AD+"attributes.ac.value"] = tok.ac + tok.ac_adjust;
+            }
+
+            //Upgrade size
             
-            //Improve AC
-            
+            //Add features
+
         }
-        
+
         //Do all updates that need done
         await item_types_remove(token, tok);                //Remove all selected item types
         await items_add(token, tok.items_to_add);           //Add all items
+        //console.log("After add")
+        
         await items_equip_all(token);                       //Equip, identify, make proficient all items
+        console.log("After equip")
+        
         await token.document.update(tok.data_to_update);    //Update all token data at once!
+        console.log("After update")
+        
         await token.actor.longRest({ dialog: false });      //Refresh spellslots and hp
+        console.log("After long rest")
 
         console.log(tok);
         console.log(token);
+        console.log(token.actor.data.data);
 
         //Cleanup token health bars
-        
-        
+
         //What race is NPC
         //  token.data.data.details.race
-        
-        
+
         //----------------------------------------------------------------------------------
 
         
@@ -220,6 +304,9 @@ async function items_add(token, items) {
         let pack = await game.packs.get(i[0]);
         let index = await pack.getIndex();
         let entry = await index.find(e => e.name === i[1]);
+        
+        //console.log(entry)
+        
         let entity = await pack.getDocument(entry._id);
         entities.push(entity.data.toObject());
     }
@@ -234,7 +321,9 @@ async function items_delete(token, items){
 
 async function items_equip_all(token){
     for (let i of token.actor.items){
-        await item_equipped_identified_proficient(token, i)
+        if (i.type == "equipment" || i.type == "weapon"){
+            await item_equipped_identified_proficient(token, i)
+        }
     }
 }
 
@@ -291,7 +380,48 @@ function roll_no1_no2(qty, d){
 }
 
 async function spells_get(tok){
-    
+    let spell_count_by_level = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 4, 2, 0, 0, 0, 0, 0, 0, 0],
+      [1, 4, 3, 0, 0, 0, 0, 0, 0, 0],
+      [1, 4, 3, 2, 0, 0, 0, 0, 0, 0],
+      [1, 4, 3, 3, 0, 0, 0, 0, 0, 0],
+      [1, 4, 3, 3, 1, 0, 0, 0, 0, 0],
+      [1, 4, 3, 3, 2, 0, 0, 0, 0, 0],
+      [1, 4, 3, 3, 3, 1, 0, 0, 0, 0],
+      [1, 4, 3, 3, 3, 2, 0, 0, 0, 0],
+      [1, 4, 3, 3, 3, 2, 1, 0, 0, 0],
+      [1, 4, 3, 3, 3, 2, 1, 0, 0, 0],
+      [1, 4, 3, 3, 3, 2, 1, 1, 0, 0],
+      [1, 4, 3, 3, 3, 2, 1, 1, 0, 0],
+      [1, 4, 3, 3, 3, 2, 1, 1, 1, 0],
+      [1, 4, 3, 3, 3, 2, 1, 1, 1, 0],
+      [1, 4, 3, 3, 3, 2, 1, 1, 1, 1],
+      [1, 4, 3, 3, 3, 3, 1, 1, 1, 1],
+      [1, 4, 3, 3, 3, 3, 2, 1, 1, 1],
+      [1, 4, 3, 3, 3, 3, 2, 2, 1, 1]
+    ];
+
+    //Add all spells per level
+    let all_spells = [];
+    let cr = tok.cr_new;
+    if (cr > 20){ cr = 20;};
+    for (let i=0; i<10; i++){
+        let spell_count = spell_count_by_level[cr][i];
+        console.log("i: " + i + ", Spell_Count: " + spell_count)
+        if (spell_count > 0){
+            let spell_list = tok.template.spell_list[i];
+            //console.log(spell_list)
+            for (let spell of spell_list){
+                //console.log(spell)
+                //all_spells.push(spell)
+                all_spells.push(spell);
+            }
+        }
+    }
+    return all_spells;
 }
 
 async function template_choose(tok){
@@ -299,33 +429,65 @@ async function template_choose(tok){
     let type = tok.opt.template_str;
 
     //Even if "generic" was chosen, use some sense to figure out what kind of NPC we are dealing with
-    if (type == "generic"){
-        //Figure out if npc can cast spells
-        if (tok.spellcasting){
-            if (tok.spellcaster_type == "wis"){
-                template.class = "cleric";
+    if (tok.type == "humanoid"){
+        if (type == "generic"){
+            //Figure out if npc can cast spells
+            if (tok.spellcasting){
+                if (tok.spellcaster_type == "wis"){
+                    template.class = "cleric";
+                } else {
+                    template.class = "wizard";
+                }            
             } else {
-                template.class = "wizard";
-            }            
-        } else {
-            template.class = "fighter";
+                template.class = "fighter";
+            }
         }
+    } else {
+        //Non-Humanoid
+        template.class = "non-humanoid";
     }
     switch(template.class){
         case "cleric":
             template.class = "Cleric";
             template.attributes = ["con","dex","wis"];
-            template.spell_list = ["Heal","Raise Dead"];
+            template.spell_list = [
+                ["Light"],
+                ["Cure Wounds","Healing Word","Inflict Wounds","Protection from Evil and Good","Sanctuary","Shield of Faith"],
+                ["Aid","Blindness/Deafness","Hold Person","Lesser Restoration","Prayer of Healing","Silence","Spiritual Weapon"],
+                ["Animate Dead","Bestow Curse","Dispel Magic","Glyph of Warding","Mass Healing Word","Magic Circle","Meld into Stone","Revivify","Water Walk"],
+                ["Banishment","Control Water","Death Ward","Freedom of Movement","Guardian of Faith","Stone Shape"],
+                ["Contagion","Dispel Evil and Good","Geas","Greater Restoration","Insect Plague","Mass Cure Wounds","Raise Dead"],
+                ["Blade Barrier","Create Undead","Harm","Heal","Word of Recall"],
+                ["Conjure Celestial","Divine Word","Etherealness","Fire Storm","Plane Shift","Regenerate","Resurrection"],
+                ["Antimagic Field","Control Weather","Earthquake"],
+                ["Gate","Mass Heal","True Resurrection"]
+            ];
             break;
         case "fighter":
             template.class = "Fighter";
             template.attributes = ["dex","wis","str"];
             template.spell_list= [];
             break;
+        case "non-humanoid":
+            template.class = "Non-Humanoid";
+            template.attributes = ["con","dex","str"];
+            template.spell_list= [];
+            break;
         case "wizard":
             template.class = "Wizard";
             template.attributes = ["con","dex","int"];
-            template.spell_list = ["Magic Missile","Fireball"];
+            template.spell_list = [
+                ["Chill Touch","Poison Spray","Ray of Frost"],
+                ["Burning Hands","Feather Fall","Mage Armor","Magic Missile","Sleep"],
+                ["Darkness","Detect Thoughts","Hold Person","Invisibility","Knock","Ray of Enfeeblement","Scorching Ray","See Invisibility","Spider Climb","Web","Levitate"],
+                ["Animate Dead","Dispel Magic","Fear","Fireball","Fly","Haste","Lightning Bolt","Slow","Vampiric Touch"],
+                ["Banishment","Black Tentacles","Greater Invisibility","Ice Storm","Phantasmal Killer","Stoneskin","Wall of Fire"],
+                ["Cloudkill","Cone of Cold","Dominate Person","Teleportation Circle","Wall of Force"],
+                ["Chain Lightning","Circle of Death","Disintegrate", "Globe of Invulnerability"],
+                ["Finger of Death","Plane Shift","Reverse Gravity","Teleport"],
+                ["Mind Blank","Maze","Power Word Stun"],
+                ["Imprisonment","Meteor Swarm","Power Word Kill"]
+            ];
             break;
     }
     console.log(template);
@@ -465,9 +627,9 @@ function dialog_options(){
               //Get all options from Form
               let opt = [];
               let e = document.getElementById("scale-npc-cr");
-              opt.npc_cr = parseInt(e.options[e.selectedIndex].value);
+              opt.cr_change = parseInt(e.options[e.selectedIndex].value);
               
-              console.log("opt.npc_cr: " + opt.npc_cr);
+              console.log("opt.cr_change: " + opt.cr_change);
               
               e = document.getElementById("npc_template");
               opt.template_str = e.options[e.selectedIndex].value;
