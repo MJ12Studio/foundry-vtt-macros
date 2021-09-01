dcc7d9529b5e61136a38f47e2d8e2298e7105e27
yahavatar <yah.avatar@gmail.com>
2021-09-01 15:26:45 -0400

    Wednesday - Commit from Work

--- src/mjspells.js
+++ src/mjspells.js
@@ -1,4 +1,16 @@
-class MJDialog{ 
+
+
+
+//==================================== Constants ====================================
+const EXPLOSION_BLUE = "modules/JB2A_DnD5e/Library/Generic/Explosion/Explosion_02_Blue_400x400.webm"
+
+const IMG_SKELETON = "systems/dnd5e/tokens/undead/Skeleton.png"
+const IMG_ZOMBIE =   "systems/dnd5e/tokens/undead/Zombie.png"
+
+const SOUND_BZS =    "sounds/big-zombie-smash-sound-effect-24270630.ogg"
+
+//===================================== Classes =====================================
+class MJdialog{ 
     constructor(header, arr_text_fn){
         var newDiv = document.createElement("div");
         newDiv.id = "mjDialog";
@@ -32,35 +44,41 @@ class MJDialog{
         document.getElementById("mjDialog").remove();
     }
 }
-class NPC{
-    constructor(compendium, npc, location){
-        (async function(){
-            let actor = await game.actors.getName(npc);
-            if (!actor){
-                //Load actor from compendium
-                console.log("importing " + npc + " from compendium...");
-                let pack = await game.packs.get(compendium);
-                let index = await pack.getIndex();
-                let entry = await index.find(e => e.name === npc);
-                game.actors.importFromCompendium(compendium, entry._id)
-                actor = game.actors.getName(npc);
-            }
-            let doc = {
-                img: actor.data.img,
-                x: location.x,
-                y: location.y,
-                vision: false,
-                hidden: false,
-                actorId: actor._id,
-                actorLink: true,
-                actorData: actor.data
-            };
-            let tok = await Token.create(doc);
-            console.log("tok:", tok);
-        })();
+class MJnpc{
+    constructor(){
+
     }
+    async create(compendium, npc, location){
+        this.x = location.x;
+        this.y = location.y;
+        let actor = await game.actors.getName(npc);
+        if (!actor){
+            //Load actor from compendium
+            console.log("importing " + npc + " from compendium...");
+            let pack = await game.packs.get(compendium);
+            let index = await pack.getIndex();
+            let entry = await index.find(e => e.name === npc);
+            game.actors.importFromCompendium(compendium, entry._id)
+            actor = game.actors.getName(npc);
+        }
+        let doc = {
+            img: actor.data.img,
+            x: location.x,
+            y: location.y,
+            vision: false,
+            hidden: false,
+            actorId: actor._id,
+            actorLink: true,
+            actorData: actor.data
+        };
+        let tok = await Token.create(doc);
+        console.log("tok:", tok);
+        this.width = tok[0].data.width;
+        this.mjCreate = true;
+    }
+
 }
-class MJToken{
+class MJtoken{
     constructor(token){
         this.actorData_updates = [];
         this.document = token.document;
@@ -72,12 +90,16 @@ class MJToken{
         this.tadd = token.actor.data.data;
         //this.x = token.x;
         //this.y = token.y;
-        console.log("token: ", this.token);
+        //console.log("token: ", this.token);
         //console.log("tadd: ", this.td);
         //console.log("tadd: ", this.tadd);
+
+        //console.log("update():", this.token.update);
+
+
     }
     ability_adjust(ability, adjust){
-        console.log("ability_adjust: " + ability + " : " + adjust);
+        //console.log("ability_adjust: " + ability + " : " + adjust);
         let current_value = this.tadd.abilities[ability].value;
         let new_value = current_value + adjust;
         if (new_value < 1){ new_value = 1; }
@@ -86,25 +108,36 @@ class MJToken{
     ability_set(ability, value){
         this.actorData_updates["actorData.data.abilities." + ability + ".value"] = value;
     }
+    actor_type_set(value){
+        this.actorData_updates["actorData.data.details.type.value"] = value;
+    }
     condition_immunity_add(ci){
         let immunities = this.tadd.traits.ci.value;
-        immunities.push(ci);
-        this.actorData_updates["actorData.data.traits.ci.value"] = immunities;
+        if (!immunities.includes(ci)){
+            immunities.push(ci);
+            this.actorData_updates["actorData.data.traits.ci.value"] = immunities;
+        }
     }
     damage_immunity_add(di){
         let immunities = this.tadd.traits.di.value;
-        immunities.push(di);
-        this.actorData_updates["actorData.data.traits.di.value"] = immunities;
+        if (!immunities.includes(di)){
+            immunities.push(di);
+            this.actorData_updates["actorData.data.traits.di.value"] = immunities;
+        }
     }
     damage_resistance_add(dr){
-        let immunities = this.tadd.traits.dr.value;
-        immunities.push(dr);
-        this.actorData_updates["actorData.data.traits.dr.value"] = immunities;
+        let resistances = this.tadd.traits.dr.value;
+        if (!resistances.includes(dr)){
+            resistances.push(dr);
+            this.actorData_updates["actorData.data.traits.dr.value"] = resistances;
+        }
     }
     damage_vulnerability_add(dv){
-        let immunities = this.tadd.traits.dv.value;
-        immunities.push(dv);
-        this.actorData_updates["actorData.data.traits.dv.value"] = immunities;
+        let resistances = this.tadd.traits.dv.value;
+        if (!resistances.includes(dv)){
+            resistances.push(dv);
+            this.actorData_updates["actorData.data.traits.dv.value"] = resistances;
+        }
     }
     explosion(file, sound){
         //Check for module, return if not
@@ -139,17 +172,21 @@ class MJToken{
     }
     async items_add() {
         //Creates raw {items_to_add_raw} out of [items_to_add_compendium] and adds them to the token
-        console.log(this.items_to_add_compendium);
+        //console.log("items_to_add_compendium:", this.items_to_add_compendium);
         for (let i of this.items_to_add_compendium){
-            console.log("item: ", i);
+            //console.log("item: ", i);
             let pack = await game.packs.get(i[0][0]);
             let index = await pack.getIndex();
             let entry = await index.find(e => e.name === i[0][1]);
             let entity = await pack.getDocument(entry._id);
             this.items_to_add_raw.push(entity.data.toObject());
         }
-        console.log(this.items_to_add_raw)
-        await token.actor.createEmbeddedDocuments("Item", this.items_to_add_raw);
+        //console.log("items_to_add_raw: ", this.items_to_add_raw)
+
+        //console.log("Adding items: actor.createED", this.token.actor)
+
+        await this.token.actor.createEmbeddedDocuments("Item", this.items_to_add_raw);
+        //await this.token.actor.addEmbeddedItems(this.items_to_add_raw);
     }
     movement_set(movement, value){
         this.actorData_updates["actorData.data.attributes.movement." + movement] = value;
@@ -174,30 +211,40 @@ class MJToken{
         this.actorData_updates["actorData.data.attributes.senses.special"] = "";
     }
     async update(){
-        console.log("update()");
-        console.log(this.actorData_updates);
-        await token.document.update(this.actorData_updates);
-        console.log("Before deleteEmbeddedDocuments")
-        await token.actor.deleteEmbeddedDocuments( "Item", this.items_to_remove );
-        console.log("before items_add()");
+        //console.log("update()");
 
+        //console.log(this.actorData_updates);
+        await this.token.update(this.actorData_updates);
+
+        //console.log("Before deleteEmbeddedDocuments");
+        //console.log("items_to_remove:", this.items_to_remove);
+        await this.token.actor.deleteEmbeddedDocuments( "Item", this.items_to_remove );
+
+        //console.log("before items_add()");
         await this.items_add();
-        token.actor.longRest({ dialog: false });
 
-        console.log(this);
+        this.token.actor.longRest({ dialog: false });
+
+        //console.log(this);
     }
 
 }
-class SFX{
-    static explosion(location, video, audio){
+class MJfx{
+    static explosion(target, video, audio){
+        //Correct for uncentered target created by MJnpc
+        if (target.mjCreate){
+            target.x += (target.width / 2) * 50;
+            target.y += (target.width / 2) * 50;
+        }
+        //console.log("NPC.explosion:", target, video, audio);
         //Check for module, return if not
         new Sequence()
         .sound(audio)
 			.volume(1.0)
         .effect()
             .file(video)
-            .atLocation(location)
-            .scale(1.5)
+            .atLocation(target)
+            .scale(1)
             .gridSize(100)
             .randomRotation()
         .play()
@@ -205,14 +252,12 @@ class SFX{
 }
 
 //if (!canvas.tokens.controlled[0]?.actor) { ui.notifications.warn("No Token selected"); return; }
-//let selected = canvas.tokens.controlled;
 
-
+//======================================= Main ======================================
 console.clear();
 console.log("mjspells");
 if (args == []){ ui.notifications.warn("Can't be run directly"); return; }
 console.log("args: ", args);
-
 
 //Caster
 let caster = canvas.tokens.controlled[0];
@@ -228,22 +273,25 @@ console.log("Spell:", spell);
 
 switch(spell){
     case "animate dead":
-        let dlg = new MJDialog("Choose Type", [["Skeleton",skeleton],["Zombie",zombie]]);
+        let dlg = new MJdialog("Choose Type", [["Skeleton",skeleton],["Zombie",zombie]]);
         function skeleton(){ animate_dead("Skeleton"); }
         function zombie(){   animate_dead("Zombie"); }
-        function animate_dead(name){
-            console.log("Targets: " + targets.length);
+        async function animate_dead(name){
             dlg.remove();
-            let location = [];
             if (targets.length == 0){
+                let location = [];
                 location.x = caster.x;
                 location.y = caster.y + 50;
-                let token = new NPC("dnd5e.monsters", name, location);
+                let target = new MJnpc();
+                await target.create("dnd5e.monsters", name, location);
+                //target.actor_type_set("Undead");
+                MJfx.explosion(target, EXPLOSION_BLUE, SOUND_BZS);
             } else {
                 for (let target of targets){
-                    let t = new MJToken(target)
+                    //console.log("target:", target);
+                    let t = new MJtoken(target)
                     t.senses_remove_all();
-                    if (zombie){
+                    if (name == "Zombie"){
                         t.ability_adjust("str",1);
                         t.ability_adjust("dex",-2);
                         t.ability_adjust("con",3);
@@ -251,7 +299,7 @@ switch(spell){
                         t.ability_adjust("wis",-2);
                         t.ability_adjust("dex",-3);
                         t.condition_immunity_add("poisoned");
-                        t.img_set("systems/dnd5e/tokens/undead/Zombie.png")
+                        t.img_set(IMG_ZOMBIE)
                         t.item_add_compendium(["dnd5e.monsterfeatures","Undead Fortitude"]);
                         t.movement_set("walk",20);
                         t.sense_add_special("passive perception 9");
@@ -262,22 +310,19 @@ switch(spell){
                         t.ability_adjust("wis",-1);
                         t.ability_adjust("dex",-3);
                         t.damage_vulnerability_add("bludgeoning");
-                        t.img_set("systems/dnd5e/tokens/undead/Skeleton.png")
+                        t.img_set(IMG_SKELETON)
                         t.movement_set("walk",30);
                         t.sense_add_special("passive perception 8");
                     }
+                    t.actor_type_set("Undead");
                     t.damage_immunity_add("poison");
                     t.item_types_remove(["feat","spell"])
                     t.name_append("(Undead)");
                     t.sense_add("darkvision", 60);
                     t.update();
-
-                    console.log("final token: ", t)
-
-                    location.x = t.x;
-                    location.y = t.y;
+                    MJfx.explosion(target, EXPLOSION_BLUE, SOUND_BZS);
                 }
-                SFX.explosion(location, "modules/JB2A_DnD5e/Library/Generic/Explosion/Explosion_02_Blue_400x400.webm", "sounds/big-zombie-smash-sound-effect-24270630.ogg");
+                
             }
         }
         break;
